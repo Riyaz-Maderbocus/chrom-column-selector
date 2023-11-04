b2cd3f77205136fed95f960a5d418c504dbb8950
Riyaz Maderbocus <madsonray0@gmail.com>
2023-11-04 12:01:08 +0000

    make column class

--- assets/js/column_chooser.js
+++ assets/js/column_chooser.js
@@ -1,3 +1,25 @@
+class Column {
+    constructor(html_id, name, img_src, diameter, maxVol, maxHeight) {
+        this.id = html_id;
+        this.name = name;
+        this.img_src = img_src;
+        this.diameter = diameter;
+
+        this.maxVol = maxVol;
+        this.maxHeight = maxHeight;
+        this.calculatedVolume = null;
+        this.caluatedHeight = null;
+
+    }
+
+    getCard() {
+        this.card = document.querySelector(`#${this.id}`);
+    }
+}
+
+const col1 = new Column("abc", "abc", "abc", 25, 250, 12);
+
+
 const omnifit = {
     radius: null,
 
@@ -11,6 +33,7 @@ const omnifit = {
 }
 const hiScale_16_20 = {
     card: document.querySelector("#hiscale_16_20"),
+    id: "#hiscale_16_20",
     colName: "HiScale 16/20",
     radius: null,
     diameter: 16,
@@ -31,6 +54,11 @@ const cardParentDiv = document.querySelector(".columns-section");
 
 const test = document.querySelector("#test");
 
+
+
+
+// utilities
+
 // Use to get the id of the sub element clicked
 // test.addEventListener("click", (e) => {
 //     console.log(this);
@@ -41,8 +69,11 @@ const test = document.querySelector("#test");
 
 // })
 
+
+//  Use this to change the inner text of the spans
 hiScale_16_20.card.querySelector(".diameter span").innerText = hiScale_16_20.diameter;
 
+// Click listener for one card
 hiScale_16_20.card.addEventListener("click", () => {
     alert("I got clicked first card")
 })
